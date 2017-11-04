7f7bd1041780a823a46086f533fb4a20cdbf60c9
stjudeslms <33370631+stjudeslms@users.noreply.github.com>
2017-11-04 16:30:21 +0530

    Initial Folder structure
    with spring mvc and mongodb dependencies

---
+++ library-mgmt-service/src/main/java/com/neotechlabs/librarymgmtservice/LibraryMgmtServiceApplication.java
@@ -0,0 +1,12 @@
+package com.neotechlabs.librarymgmtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMgmtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMgmtServiceApplication.class, args);
+	}
+}
